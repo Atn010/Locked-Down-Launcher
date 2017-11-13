7f9778520268369e21653a3ff1c64a386c1da65a
Atn010G <atn010g@gmail.com>
2017-11-13 09:47:52 +0700

    Added Package List

--- SimpleLauncher/src/main/java/ch/arnab/simplelauncher/MainActivity.java
+++ SimpleLauncher/src/main/java/ch/arnab/simplelauncher/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayList <String> appList = new ArrayList<>();
 
 
         final ListView listApplication = (ListView)findViewById(R.id.listApplication);
@@ -34,6 +35,8 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
 
+                appList.clear();
+
                 StringBuilder result = new StringBuilder();
                 for(int i=0;i<adapter.mCheckStates.size();i++)
                 {
@@ -42,11 +45,12 @@ public class MainActivity extends Activity {
 
                         result.append(app_info[i].applicationName);
                         result.append("\n");
+                        appList.add(app_info[i].applicationName);
                     }
 
                 }
                 System.out.println(result);
-                Log.i("Result", "onClick: " + result);
+                Log.i("Result", "onClick: " + printArrayList(appList));
                 Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
             }
 
@@ -97,5 +101,17 @@ public class MainActivity extends Activity {
         adapter = new AppInfoAdapter(this, R.layout.listview_item_row, app_info);
         listApplication.setAdapter(adapter);
 
+    }
+
+    private String printArrayList(ArrayList<String> appList) {
+        String listBuild = null;
+        for(int i = 0; i<appList.size();i++){
+            if(i == 0){
+                listBuild = appList.get(i);
+            }else {
+                listBuild = listBuild + " | " + appList.get(i);
+            }
+        }
+        return listBuild;
     }
 }
