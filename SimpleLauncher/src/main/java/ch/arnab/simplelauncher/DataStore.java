08d3df740af5f1a1fc7b27503f9e50857528f7c1
Atn010G <atn010g@gmail.com>
2018-01-24 10:33:52 +0700

    added comments

--- SimpleLauncher/src/main/java/ch/arnab/simplelauncher/DataStore.java
+++ SimpleLauncher/src/main/java/ch/arnab/simplelauncher/DataStore.java
@@ -26,9 +26,11 @@ class DataStore {
 
     private DataStore() {
         if(adminPassword == null) {
+            /// if no password is set, use 'admin' as password
             adminPassword = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
         }
         if(appList == null) {
+            /// if no applist, array list is empty
             appList = new ArrayList<String>();
         }
 
@@ -36,9 +38,12 @@ class DataStore {
     String adminPassword;
     ArrayList<String> appList;
 
-    //private
 
-
+    /**
+     * Hash string
+     * @param message the string to be hashed
+     * @return a hash of the message
+     */
     public static String hashString(String message) {
 
         try {
@@ -53,6 +58,11 @@ class DataStore {
         return message;
     }
 
+    /**
+     * convert  Byte Array to Hex
+     * @param arrayBytes array of bytes
+     * @return hex characters
+     */
     private static String convertByteArrayToHexString(byte[] arrayBytes) {
         StringBuffer stringBuffer = new StringBuffer();
         for (int i = 0; i < arrayBytes.length; i++) {
@@ -62,6 +72,11 @@ class DataStore {
         return stringBuffer.toString();
     }
 
+    /**
+     * Saves all content of applist and password into preference
+     * @param context context of the class that calls this function
+     * @return return nothing
+     */
     public String save(Context context){
         Set<String> set = new HashSet<String>(appList);
         String look = "saved";
@@ -73,6 +88,11 @@ class DataStore {
         return look;
     }
 
+    /**
+     * Load all of the content of the applist and password  from the preference
+     * @param context context of the class that calls this function
+     * @return return nothing
+     */
     public String load(Context context){
         System.out.println("File is loaded");
         String look = "load";
